f7cc304e3ba4a7caef3995c70cdb2a9f0b0a426a
joatienza <joatienza@ctemc.org>
2016-05-08 21:16:07 -0400

    stuff

--- uas_skeleton_march13.js
+++ uas_skeleton_march13.js
@@ -200,3 +200,207 @@ $(document).ready(function(){
     };
   });
 });*/
+
+function cycleBackgrounds() {
+  var index = 0;
+
+  $imageEls = $('.toggle-image'); // Get the images to be cycled.
+
+  setInterval(function () {
+    // Get the next index.  If at end, restart to the beginning.
+    index = index + 1 < $imageEls.length ? index + 1 : 0;
+    // Show the next image.
+    $imageEls.eq(index).addClass('show');
+    // Hide the previous image.
+    $imageEls.eq(index - 1).removeClass('show');
+
+  }, 5000);
+};
+
+// Document Ready.
+$(function () {
+  cycleBackgrounds();
+});
+
+
+function cycleBackgrounds2() {
+  var index2 = 0;
+
+  $imageEls2 = $('.toggle-image2'); // Get the images to be cycled.
+
+  setInterval(function () {
+    // Get the next index.  If at end, restart to the beginning.
+    index2 = index2 + 1 < $imageEls2.length ? index2 + 1 : 0;
+    // Show the next image.
+    $imageEls2.eq(index2).addClass('show');
+    // Hide the previous image.
+    $imageEls2.eq(index2 - 1).removeClass('show');
+
+  }, 6000);
+};
+
+// Document Ready.
+$(function () {
+  cycleBackgrounds2();
+});
+
+function switchVisible1() {
+  var modal1 = document.getElementById('courseDescriptionPageIED');
+  var modal2 = document.getElementById('mediaPageIED');
+  var modal3 = document.getElementById('teachNotePageIED');
+
+  if (modal1) {
+
+      if (modal1 == 'none') {
+
+          modal1.style.display = 'block';
+          modal2.style.display = 'none';
+      }
+
+      else {
+          modal1.style.display = 'none';
+          modal2.style.display = 'block';
+      }
+  }
+
+}
+
+function switchVisible2() {
+  var modal1 = document.getElementById('courseDescriptionPageIED');
+  var modal2 = document.getElementById('mediaPageIED');
+  var modal3 = document.getElementById('teachNotePageIED');
+
+  if (modal1) {
+
+      if (modal1 == 'none') {
+        
+          modal1.style.display = 'block';
+          modal3.style.display = 'none';
+      }
+
+      else {
+          modal1.style.display = 'none';
+          modal3.style.display = 'block';
+      }
+  }
+
+}
+
+function switchVisible3() {
+  var modal1 = document.getElementById('courseDescriptionPageIED');
+  var modal2 = document.getElementById('mediaPageIED');
+  var modal3 = document.getElementById('teachNotePageIED');
+
+  if (modal2) {
+
+      if (modal2 == 'none') {
+
+          modal2.style.display = 'block';
+          modal3.style.display = 'none';
+      }
+
+      else {
+          modal2.style.display = 'none';
+          modal3.style.display = 'block';
+      }
+  }
+
+}
+
+function switchVisible4() {
+  var modal1 = document.getElementById('courseDescriptionPageIED');
+  var modal2 = document.getElementById('mediaPageIED');
+  var modal3 = document.getElementById('teachNotePageIED');
+
+  if (modal2) {
+
+      if (modal2 == 'none') {
+        
+          modal2.style.display = 'block';
+          modal1.style.display = 'none';
+      }
+
+      else {
+          modal2.style.display = 'none';
+          modal1.style.display = 'block';
+      }
+  }
+
+}
+
+function switchVisible5() {
+  var modal1 = document.getElementById('courseDescriptionPageIED');
+  var modal2 = document.getElementById('mediaPageIED');
+  var modal3 = document.getElementById('teachNotePageIED');
+
+  if (modal3) {
+
+      if (modal3 == 'none') {
+
+          modal3.style.display = 'block';
+          modal1.style.display = 'none';
+      }
+
+      else {
+          modal3.style.display = 'none';
+          modal1.style.display = 'block';
+      }
+  }
+
+}
+
+function switchVisible6() {
+  var modal1 = document.getElementById('courseDescriptionPageIED');
+  var modal2 = document.getElementById('mediaPageIED');
+  var modal3 = document.getElementById('teachNotePageIED');
+
+  if (modal3) {
+
+      if (modal3 == 'none') {
+        
+          modal3.style.display = 'block';
+          modal2.style.display = 'none';
+      }
+
+      else {
+          modal3.style.display = 'none';
+          modal2.style.display = 'block';
+      }
+  }
+
+}
+
+/*Changing Backgrounds*/
+  $(function() {
+    var courseyr1A = $('#Half1A');
+    var backgroundsyr1A = 
+           ['url(ctePics/measure.jpg) no-repeat center center fixed', 
+            'url(ctePics/juniorPic.jpg)',
+            'url(ctePics/motherboard.jpg) no-repeat center center fixed'];
+    var currentyr1A = 0;
+    function nextBackground1A() {
+      courseyr1A.css(
+       'background',
+        backgroundsyr1A[currentyr1A = ++currentyr1A % backgroundsyr1A.length]
+      );
+      setTimeout(nextBackground1A, 4000);
+    }
+    setTimeout(nextBackground1A, 4000);
+    courseyr1A.css('background', backgroundsyr1A[0]);
+
+    var courseyr1B = $('#Half1B');
+    var backgroundsyr1B = 
+           ['url(ctePics/keyboard.jpg) no-repeat center center fixed', 
+            'url(ctePics/spring.jpg)',
+            'url(ctePics/office.jpg) no-repeat center center fixed'];
+    var currentyr1B = 0;
+    function nextBackground1B() {
+      courseyr1B.css(
+       'background',
+        backgroundsyr1B[currentyr1B = ++currentyr1B % backgroundsyr1B.length]
+      );
+      setTimeout(nextBackground1B, 5000);
+    }
+    setTimeout(nextBackground1B, 5000);
+    courseyr1B.css('background', backgroundsyr1B[0]);
+  });
